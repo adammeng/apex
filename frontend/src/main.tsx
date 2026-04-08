89f499c1e0653cb1402ef164a221e7dac9491d78
祁萌 <qimeng@digitalchina.com>
2026-04-08 15:48:50 +0800

    chore: 移除前端 vconsole 调试代码

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import VConsole from 'vconsole'
 import App from './App.tsx'
 import './index.css'
-
-function shouldEnableVConsole() {
-  if (typeof window === 'undefined') {
-    return false
-  }
-
-  const params = new URLSearchParams(window.location.search)
-  if (params.get('vconsole') === '1') {
-    return true
-  }
-
-  return /Lark|Feishu/i.test(window.navigator.userAgent)
-}
-
-if (shouldEnableVConsole()) {
-  const vconsole = new VConsole()
-  ;(window as Window & { __APEX_VCONSOLE__?: VConsole }).__APEX_VCONSOLE__ = vconsole
-  console.info('[apex-debug] vConsole enabled')
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
